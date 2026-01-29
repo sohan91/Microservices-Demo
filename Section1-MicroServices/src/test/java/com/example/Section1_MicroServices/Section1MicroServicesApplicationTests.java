00940131337729c34ab0846611b5c4708deddd63
sohan <sohanprasad2003@gmail.com>
2026-01-29 11:28:18 +0530

    create initial project

---
+++ Section1-MicroServices/src/test/java/com/example/Section1_MicroServices/Section1MicroServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Section1_MicroServices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Section1MicroServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
